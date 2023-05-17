0e136708b843cd2ff6ad329438c7e54c6fd014b5
vincentvgit <127845446+vincentvgit@users.noreply.github.com>
2023-05-17 09:54:08 +0200

    Update vms.tf

--- vms.tf
+++ vms.tf
@@ -28,7 +28,7 @@
    vm_size               = "Standard_DS1_v2"
 
    # Uncomment this line to delete the OS disk automatically when deleting the VM
-   # delete_os_disk_on_termination = true
+    delete_os_disk_on_termination = true
 
    # Uncomment this line to delete the data disks automatically when deleting the VM
    delete_data_disks_on_termination = true
